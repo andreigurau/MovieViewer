3372f9583ba2f403982b2bd69d90f40b62b21b99
Andrei Gurau <andreiyugurau@gmail.com>
2016-01-15 21:37:20 -0500

    Submission one optional

--- MoviesViewController.swift
+++ MoviesViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 import AFNetworking
 class MoviesViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    @IBOutlet weak var networkErrorView: UIView!
+    
     @IBOutlet var scrollView: UIScrollView!
     
     @IBOutlet weak var tableView: UITableView!
-    var refreshControl: UIRefreshControl!
+    
     
     var movies: [NSDictionary]?
     
@@ -23,6 +25,10 @@ class MoviesViewController: UIViewController,UITableViewDataSource,UITableViewDe
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
+        networkErrorView.hidden = true
         
         tableView.dataSource = self
         tableView.delegate = self
@@ -48,12 +54,18 @@ class MoviesViewController: UIViewController,UITableViewDataSource,UITableViewDe
                             self.tableView.reloadData()
                     }
                 }
+                else
+                {
+                    //self.networkErrorView.hidden = false
+                    EZLoadingActivity.hide(success: false, animated: false)
+                    self.networkErrorView.hidden = false
+                }
         });
         task.resume()
         // Do any additional setup after loading the view.
-        refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
-        tableView.insertSubview(refreshControl, atIndex: 0)
+        //refreshControl = UIRefreshControl()
+        //refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        //tableView.insertSubview(refreshControl, atIndex: 0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -96,11 +108,59 @@ class MoviesViewController: UIViewController,UITableViewDataSource,UITableViewDe
             dispatch_get_main_queue(), closure)
     }
     
-    func onRefresh() {
+    /*func onRefresh() {
         delay(2, closure: {
             self.refreshControl.endRefreshing()
         })
+    }*/
+    
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        EZLoadingActivity.show("Loading...", disableUI: true)
+        // Make network request to fetch latest data
+        networkErrorView.hidden = true
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                            
+                            self.movies = responseDictionary["results"] as! [NSDictionary]
+                            self.tableView.reloadData()
+                            EZLoadingActivity.hide(success: true, animated: false)
+                    }
+                }
+                else
+                {
+                    //self.networkErrorView.hidden = false
+                    EZLoadingActivity.hide(success: false, animated: false)
+                    self.networkErrorView.hidden = false
+                }
+        });
+        task.resume()
+
+        // Do the following when the network request comes back successfully:
+        // Update tableView data source
+        self.tableView.reloadData()
+        refreshControl.endRefreshing()
     }
+    
+    
 
     /*
     // MARK: - Navigation
